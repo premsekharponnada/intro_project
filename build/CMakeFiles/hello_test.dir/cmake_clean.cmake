a936706018a1453dbaffc879e090c7f3a0b68c81
PREM SEKHAR <84108418+premsekharponnada@users.noreply.github.com>
2021-06-10 17:24:42 +0530

    included required code

---
+++ build/CMakeFiles/hello_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/hello_test"
+  "../bin/hello_test.pdb"
+  "CMakeFiles/hello_test.dir/test.cc.o"
+  "CMakeFiles/hello_test.dir/test.cc.o.d"
+  "hello_test[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
