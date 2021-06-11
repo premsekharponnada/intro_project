a987f4309fd6b0d1d95d587e28aec02e992de6ef
PREM SEKHAR <84108418+premsekharponnada@users.noreply.github.com>
2021-06-11 07:29:27 +0530

    deleted unwanted build and bin files

--- build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( HelloTest.BasicAssertions /Users/premsekhar/Desktop/intro_project/bin/hello_test [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties( HelloTest.BasicAssertions PROPERTIES WORKING_DIRECTORY /Users/premsekhar/Desktop/intro_project/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( hello_test_TESTS HelloTest.BasicAssertions)
